610fb8202c0f3de8efc00f2b3bc045a427b0b78f
Sairaj Bhakare <bhakaresairaj@gmail.com>
2024-02-27 08:44:41 +0530

    Initial commit

---
+++ 1. K Nearest Neighbors/knn.py
@@ -0,0 +1,116 @@
+import sys
+import numpy as np
+import pickle
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from MNIST_Dataset_Loader.mnist_loader import MNIST
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+# Load MNIST data
+print('\nLoading MNIST Data...')
+data = MNIST('./MNIST_Dataset_Loader/dataset/')
+
+# Load training data
+print('\nLoading Training Data...')
+img_train, labels_train = data.load_training()
+train_img = np.array(img_train)
+train_labels = np.array(labels_train)
+
+# Load testing data
+print('\nLoading Testing Data...')
+img_test, labels_test = data.load_testing()
+test_img = np.array(img_test)
+test_labels = np.array(labels_test)
+
+# Features
+X = train_img
+
+# Labels
+y = train_labels
+
+# Prepare Classifier Training and Validation Data
+print('\nPreparing Classifier Training and Validation Data...')
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
+
+# KNN Classifier
+print('\nKNN Classifier with n_neighbors = 5, algorithm = auto, n_jobs = 10')
+print('\nPickling the Classifier for Future Use...')
+clf = KNeighborsClassifier(n_neighbors=5, algorithm='auto', n_jobs=10)
+clf.fit(X_train, y_train)
+
+# Save the trained classifier
+with open('MNIST_KNN.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+# Load the trained classifier
+pickle_in = open('MNIST_KNN.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+# Accuracy of the trained classifier
+print('\nCalculating Accuracy of trained Classifier...')
+confidence = clf.score(X_test, y_test)
+
+# Making Predictions on Validation Data
+print('\nMaking Predictions on Validation Data...')
+y_pred = clf.predict(X_test)
+
+# Accuracy of Predictions
+print('\nCalculating Accuracy of Predictions...')
+accuracy = accuracy_score(y_test, y_pred)
+
+# Confusion Matrix
+print('\nCreating Confusion Matrix...')
+conf_mat = confusion_matrix(y_test, y_pred)
+
+# Display results
+print('\nKNN Trained Classifier Confidence: ', confidence)
+print('\nPredicted Values: ', y_pred)
+print('\nAccuracy of Classifier on Validation Image Data: ', accuracy)
+print('\nConfusion Matrix: \n', conf_mat)
+
+# Plot Confusion Matrix Data as a Matrix
+plt.matshow(conf_mat)
+plt.title('Confusion Matrix for Validation Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+# Making Predictions on Test Input Images
+print('\nMaking Predictions on Test Input Images...')
+test_labels_pred = clf.predict(test_img)
+
+# Accuracy of the Trained Classifier on Test Data
+print('\nCalculating Accuracy of Trained Classifier on Test Data... ')
+acc = accuracy_score(test_labels, test_labels_pred)
+
+# Confusion Matrix for Test Data
+print('\n Creating Confusion Matrix for Test Data...')
+conf_mat_test = confusion_matrix(test_labels, test_labels_pred)
+
+# Display results
+print('\nPredicted Labels for Test Images: ', test_labels_pred)
+print('\nAccuracy of Classifier on Test Images: ', acc)
+print('\nConfusion Matrix for Test Data: \n', conf_mat_test)
+
+# Plot Confusion Matrix for Test Data
+plt.matshow(conf_mat_test)
+plt.title('Confusion Matrix for Test Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.axis('off')
+plt.show()
+
+# Show the Test Images with Original and Predicted Labels
+a = np.random.randint(1, 50, 20)
+for i in a:
+    two_d = (np.reshape(test_img[i], (28, 28)) * 255).astype(np.uint8)
+    plt.title('Original Label: {0}  Predicted Label: {1}'.format(test_labels[i], test_labels_pred[i]))
+    plt.imshow(two_d, interpolation='nearest', cmap='gray')
+    plt.show()
+
